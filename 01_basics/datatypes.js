f71b32a36b9671aed00fbd7dc28b1823f172feb6
suraj9196 <70427081+suraj9196@users.noreply.github.com>
2023-11-27 13:33:30 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+// alert(3+3) // we are using nodejs, not browser
+let age = 20
+//datetypes
+//number    2 to power 53
+//bigint   for very big number
+//string
+//boolean
+//null    standalone value
+//undefines  nothing have 
+//symbol => unique
+
+//object
+
+console.log(typeof "suraj");  //string
+console.log(typeof age);      // number
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
